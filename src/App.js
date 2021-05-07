084ec5e88b7285b44565794718025e7cbd5b3fb7
Mateusz <mateuszek1029@gmail.com>
2021-05-07 10:20:10 +0200

    “LAB_09”

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/home";
 import NavBar from "./components/common/navbar";
 import NotFound from "./components/notFound";
 import Redirect from "react-router-dom/es/Redirect";
+import LoginForm from "./components/loginHome";
 
 function App() {
     return (
@@ -13,12 +14,12 @@ function App() {
             <div className="container">
                 <div className="content">
                     <Switch>
-                        <Route path="/posts"
-                               render={props => <Posts sortBy="newest" {...props} />}
-                        />
-                        <Route path="/not-found" exact component={NotFound} />
+                        <Route path="/login" component={LoginForm} />
+                        <Route path="/posts" component={Posts}/>
+                        <Route path="/not-found" component={NotFound} />
                         <Route path="/" exact component={Home} />
                         <Redirect to="/not-found" />
+
                     </Switch>
                 </div>
             </div>
