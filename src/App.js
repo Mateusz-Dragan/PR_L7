9d50b3d843850d681f06ec295b26b0605ab9dd0a
Mateusz <mateuszek1029@gmail.com>
2021-05-28 10:53:05 +0200

    LAB_12

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Posts from "./components/posts";
 import Home from "./components/home";
 import NavBar from "./components/common/navbar";
 import NotFound from "./components/notFound";
+import SignUpForm from "./components/signUp";
 import Redirect from "react-router-dom/es/Redirect";
 import LoginForm from "./components/loginHome";
+import { isExpired } from "react-jwt";
 
 function App() {
     return (
@@ -15,9 +17,17 @@ function App() {
                 <div className="content">
                     <Switch>
                         <Route path="/login" component={LoginForm} />
-                        <Route path="/posts" component={Posts}/>
+                        <Route path="/posts"
+                               render={props => {
+                                   if (isExpired(localStorage.getItem('token'))) {
+                                       return <Redirect to="/" />;
+                                   }
+                                   return <Posts />;
+                               }}
+                        />
                         <Route path="/not-found" component={NotFound} />
                         <Route path="/" exact component={Home} />
+                        <Route path="/signUp" component={SignUpForm} />
                         <Redirect to="/not-found" />
 
                     </Switch>
